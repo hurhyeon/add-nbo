5390f1b7d9f7d226f04ded6d80a5acab9bd4bea5
hurhyeon <nsksjsk2@hanmail.net>
2023-07-05 03:57:49 -0400

    add-nbo.cpp

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -3,12 +3,10 @@
 #include <stdio.h>
 #include <netinet/in.h>
 
-void add(uint8_t buf1[], uint8_t buf2[]) {
-    uint32_t* p1 = reinterpret_cast<uint32_t*>(buf1);
-    uint32_t n1 = ntohl(*p1);
+void add(uint32_t buf1, uint32_t buf2) {
+    uint32_t n1 = htonl(buf1);
 
-    uint32_t* p2 = reinterpret_cast<uint32_t*>(buf2);
-    uint32_t n2 = ntohl(*p2);
+    uint32_t n2 = htonl(buf2);
 
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",n1, n1, n2, n2, n1+n2, n1+n2);
 }
@@ -19,10 +17,10 @@ int main(int argc, char *argv[]) {
         printf("sample : add-nbo a.bin c.bin \n");
         return 0;
     }
-    uint8_t buffer1[32], buffer2[32]; 
+    uint32_t buffer1, buffer2; 
     FILE *f1, *f2;
     f1 = fopen(argv[1], "rb"); f2 = fopen(argv[2], "rb");
-    fread(buffer1, 1, 32, f1); fread(buffer2, 1, 32, f2);
+    fread(&buffer1, sizeof(uint32_t), 1, f1); fread(&buffer2, sizeof(uint32_t), 1, f2);
 
     add(buffer1, buffer2);
     fclose(f1); fclose(f2);
